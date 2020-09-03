54a692e3512eac24e5cc38335cd093042b06bf2b
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2020-09-03 00:50:10 -0500

    Add Gaussian kernel

--- Filters/ass1.py
+++ Filters/ass1.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import numpy as np
 import cv2 as cv
 
-
+a = 0
 def main():
     # add paths to the input images
     path = Path.cwd() / 'res'
@@ -24,7 +24,7 @@ def main():
     
     # generate all kernels according to given size
     meanKernel = getKernel('mean', kernelSize)
-    gaussianKernel = getKernel('gaussian', kernelSize)
+    gaussianKernel = getKernel('gaussian', kernelSize, 1)
     sharpenKernel = getKernel('sharpen', kernelSize)
     
     convolution(lenaImg, meanKernel)
@@ -40,16 +40,32 @@ def main():
     medianFilter(artImg, kernelSize)
     
     
-def getKernel(name, size):
+def getKernel(name, size, sigma=-1):
     if name == 'mean':
-        kernel = np.ones(shape=(size, size), dtype='float32')
+        kernel = np.ones((size, size))
         kernel *= 1/(size**2)
-        print(kernel)
     elif name == 'gaussian':
-        pass
+        s = 2 * sigma**2
+        denom = 1 / (np.pi * s)
+        kernel = np.full((size, size), denom)
+        
+        x = np.arange(-(size//2), size//2+1)
+        y = np.arange(-(size//2), size//2+1)
+        xx, yy = np.meshgrid(y, x)
+        kernel *= np.exp(-(xx**2 + yy**2) / s)
+        print(kernel)
+        
+        """
+        old way to compute gaussian kernel
+        for x in range(size):
+            for y in range(size):
+                kernel[x, y] *= np.exp(-((x-size//2)**2 + (y-size//2)**2) / s)
+        """
+
     elif name == 'sharpen':
         pass
-    
+    global a
+    a = kernel
     return kernel
     
     
